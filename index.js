16b298681ef68af47af87267719d97a719056b24
princetonk12 <manish@princetonk12.org>
2022-04-08 11:26:48 -0400

    Added a website and documentation to the project.

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const proxy = new Corrosion({
 proxy.bundleScripts();
 
 server.on('request', (request, response) => {
+    console.log(request.url)
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
-    response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
+    else if (request.url == '/chrome.html' || request.url == '/docs/chrome.html') return response.end(fs.readFileSync(__dirname + '/chrome.html', 'utf-8'));
+    else if (request.url == '/') return response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
+    else if (request.url == '/docs/') return response.end(fs.readFileSync(__dirname + '/docs/index.html', 'utf-8'));
+    else if (request.url == '/docs/local-development') return response.end(fs.readFileSync(__dirname + '/docs/local-dev.html', 'utf-8'));
+    else if (request.url == '/docs/deploy-clone') return response.end(fs.readFileSync(__dirname + '/docs/deploy-clone.html', 'utf-8'));
+    else if (request.url == '/contact/') return response.end(fs.readFileSync(__dirname + '/contact.html', 'utf-8'));
 }).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(PORT); // port other than 443 if it is needed by other software.
